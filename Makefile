d8c47bf84bf7d8c6cb30c78d09fd2875293c8b69
Sascha Peilicke <sascha@peilicke.de>
2018-07-14 15:06:48 +0200

    Add configuration file handling
    OS-neutral by using XDG path specification. Currently only persists the
    user ID. Going forward, can be used for unit system (metric / imperial),
    etc.

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 # Simple Makefile for hound-cli
 
 all:
+	go get github.com/casimir/xdg-go
 	go get github.com/soundhound/houndify-sdk-go
+	go get github.com/spf13/viper
 	go build hound.go
 
 clean:
--- hound.go
+++ hound.go
@@ -6,45 +6,87 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/casimir/xdg-go"
 	"github.com/soundhound/houndify-sdk-go/houndify"
+	"github.com/spf13/viper"
 )
 
 const (
+	appName           = "hound-cli"
 	houndifyClientId  = "***REMOVED***"
 	houndifyClientKey = "***REMOVED***"
 )
 
-// Creates a pseudo unique / random request ID.
-func createRequestID() string {
+// Creates a pseudo unique / random string.
+func randString() string {
 	n := 10
 	b := make([]byte, n)
 	rand.Read(b)
 	return fmt.Sprintf("%X", b)
 }
 
+// Creates a valid user ID.
 func userId() string {
 	userId := os.Getenv("USER")
 	if len(userId) == 0 {
-		userId = "hound"
+		userId = "hound-" + randString()
 	}
 	return userId
+}
+
+func readConfig() *viper.Viper {
+	// Ensure config file exists to pacify Viper
+	xdgApp := xdg.App{Name: appName}
+	configFile := xdgApp.ConfigPath(appName + ".yaml")
+
+	if _, err := os.Stat(configFile); os.IsNotExist(err) {
+		configFileDir := filepath.Dir(configFile)
+		if err := os.MkdirAll(configFileDir, 0755); err != nil {
+			panic(err)
+		}
+		if f, err := os.Create(configFile); err != nil {
+			panic(err)
+		} else {
+			defer f.Close()
+		}
+	}
+
+	// Load configuration
+	v := viper.New()
+	v.SetConfigName(appName)
+	v.AddConfigPath(xdgApp.ConfigPath("")) // path to look for the config file in
+	v.SetConfigType("yaml")
+
+	v.SetDefault("User", userId())
+
+	err := v.ReadInConfig() // Find and read the config file
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	return v
 }
 
 func main() {
 	flagVerbose := flag.Bool("v", false, "Verbose mode")
 	flag.Parse()
 
-	// Make log not print out time info
 	log.SetFlags(0)
 
+	// Configuration
+	config := readConfig()
+
+	// Houndify
 	houndifyClient := houndify.Client{
 		ClientID:  houndifyClientId,
 		ClientKey: houndifyClientKey,
 		Verbose:   *flagVerbose,
 	}
 	houndifyClient.EnableConversationState()
+	//TODO: houndifyClient.SetConversationState()
 
 	query := strings.Join(flag.Args(), " ")
 	if len(query) == 0 {
@@ -53,8 +95,8 @@ func main() {
 
 	req := houndify.TextRequest{
 		Query:             query,
-		UserID:            userId(),
-		RequestID:         createRequestID(),
+		UserID:            config.GetString("User"),
+		RequestID:         randString(),
 		RequestInfoFields: make(map[string]interface{}),
 	}
 
@@ -67,4 +109,7 @@ func main() {
 		log.Fatalf("Failed to understand houndify's response:\n%s\n", serverResponse)
 	}
 	fmt.Println(writtenResponse)
+
+	//TODO: config.Set("State", houndifyClient.GetConversationState())
+	config.WriteConfig()
 }
